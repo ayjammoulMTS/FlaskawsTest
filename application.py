07e7a9b01d12dd6c62d6a850a541a5bb9a1b4f5e
ayjammoulMTS <jammoul.171@gmail.com>
2022-10-03 13:58:12 +0300

    kast

--- application.py
+++ application.py
@@ -2,7 +2,13 @@ from flask import Flask
 from backend import create_app
 
 
-application = create_app()
+
+if __name__ == "__main__":
+    application = create_app()
+    application.run(debug=True)
+
+else:
+    application = create_app()
 # application.run()
 
 
