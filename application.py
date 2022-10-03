ac040070573e9767f0e176f9cc0c907c3588536d
ayjammoulMTS <jammoul.171@gmail.com>
2022-10-03 13:10:48 +0300

    owjrb

--- application.py
+++ application.py
@@ -2,7 +2,12 @@ from flask import Flask
 from testProject.app import create_app
 
 
-application = create_app()
-application.run()
+# application = create_app()
+# application.run()
 
 
+application = Flask(__name__)
+
+@application.route('/')
+def firstEnd():
+    return ("Success Worked")
